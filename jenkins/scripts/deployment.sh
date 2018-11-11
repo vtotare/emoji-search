ca57e504bf06419e76156816ab9fc853c39b5ab6
vtotare <totare.vishal@gmail.com>
2018-11-11 23:38:36 +0530

    update file with appropriate information

--- jenkins/scripts/deployment.sh
+++ jenkins/scripts/deployment.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command builds your Node.js/React application for'
+echo 'The following "npm" command builds your Node.js application for'
 echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
+echo '"/var/jenkins_home/workspace/Prudential_SRE_Case" directory),'
 echo 'correctly bundles React in production mode and optimizes the build for'
 echo 'the best performance.'
 set -x
 npm run build
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
+echo 'The following "npm" command runs your Node.js application in'
 echo 'development mode and makes the application available for web browsing.'
 echo 'The "npm start" command has a trailing ampersand so that the command runs'
 echo 'as a background process (i.e. asynchronously). Otherwise, this command'
@@ -24,6 +24,6 @@ echo $! > .pidfile
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
+echo 'Visit http://<servername>:3000 to see your Node.js application in action.'
 echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
 echo 'created your initial Pipeline as a Jenkinsfile.)'
