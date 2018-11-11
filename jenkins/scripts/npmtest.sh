855f7d7b0211b82a93d36bb97a797f11a55dbed2
vtotare <totare.vishal@gmail.com>
2018-11-11 16:32:57 +0530

    Update in requisite files for npm test

---
+++ jenkins/scripts/npmtest.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+echo 'The following "npm" command tests that your simple Node.js'
+echo 'application renders satisfactorily. This command actually invokes the test'
+set -x
+npm test
